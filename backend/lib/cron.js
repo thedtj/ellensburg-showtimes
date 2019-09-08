515a9f4e913c4a7ad4e7e891715394c7d3500198
thedtj <awesomedan@gmail.com>
2019-09-07 18:04:07 -0700

    db Update

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -6,6 +6,7 @@ cron.schedule('0 21-23,0-12 * * THU-FRI', () => {
 	runCron()
 })
 
+// * Constant scraping for use during dev
 // cron.schedule('* * * * *', () => {
 // 	console.log('Cron running. Fun fact, Unicron was played by Orson Welles.')
 // 	runCron()

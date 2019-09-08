f4ad4be6d6cf8ad47df8d34055a083ec98515f61
thedtj <awesomedan@gmail.com>
2019-09-07 19:15:26 -0700

    esm resetup

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,3 @@
-import express from 'express'
-import { getHTML, getDates, getShowtimes } from './lib/scraper'
-import './lib/cron'
+require = require('"esm')(module)
 
-const app = express()
-
-app.get('/', async (req, res, next) => {
-
-	const showtimesHTML = await getHTML(
-		'http://ellensburgmovies.com/gmc_html/gmc_html_showtimes.html'
-	)
-
-	const [dates, { movies, comingSoon }] = await Promise.all([
-		getDates(showtimesHTML),
-		getShowtimes(showtimesHTML)
-	])
-
-	res.json({ dates, movies, comingSoon })
-})
-
-const port = process.eventNames.PORT || 5800
-
-app.listen(port, () => console.log(`app running on port ${port}`))
+module.exports = require('./main.js')
---
+++ backend/main.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { getHTML, getDates, getShowtimes } from './lib/scraper'
+import './lib/cron'
+
+const app = express()
+
+app.get('/', async (req, res, next) => {
+
+	const showtimesHTML = await getHTML(
+		'http://ellensburgmovies.com/gmc_html/gmc_html_showtimes.html'
+	)
+
+	const [dates, { movies, comingSoon }] = await Promise.all([
+		getDates(showtimesHTML),
+		getShowtimes(showtimesHTML)
+	])
+
+	res.json({ dates, movies, comingSoon })
+})
+
+const port = process.eventNames.PORT || 5800
+
+app.listen(port, () => console.log(`app running on port ${port}`))
+
+export default app
